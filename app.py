e9cee5ec21ad04a67ee3d67811425f99df85e8c2
isabarrenha <barrenha95@yahoo.com.br>
2024-08-22 09:25:58 -0300

    get-html-into-streamlit

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+path_to_html = "./content/portfolio.html"
+
+with open(path_to_html, 'r') as f:
+    html_data = f.read()
+
+st.header("Show an external HTML")
+st.components.v1.html(html_data, scrolling=True, height=500)
+st.markdown(html_data, unsafe_allow_html=True)
+
+#st.write('Hello world!')
