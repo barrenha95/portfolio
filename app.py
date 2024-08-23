8e13ec4d1be4ef59aa2ccdd4a64e2963e79a7324
isabarrenha <barrenha95@yahoo.com.br>
2024-08-23 09:31:27 -0300

    add-config-file

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ with open(path_to_html, 'r') as f:
 
 st.header("Show an external HTML")
 st.components.v1.html(html_data, scrolling=True, height=500)
-st.markdown(html_data, unsafe_allow_html=True)
+#st.markdown(html_data, unsafe_allow_html=True)
 
 #st.write('Hello world!')
